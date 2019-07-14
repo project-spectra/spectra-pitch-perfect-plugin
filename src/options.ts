4e94c84a3eff3a42ff5aa69a5ec40a7044e1e9ea
Bryan Kok <bryan.wyern1@gmail.com>
2019-07-15 05:07:37 +0800

    Add interface declaration for the callback upon pitch detection

---
+++ src/options.ts
@@ -0,0 +1,8 @@
+export interface SpectraPitchPerfectOptions {
+
+    /**
+     * Accepts 3 arguments pitchHz: Number, probability: Number, isPitched: Boolean
+     */
+    onPitchDetectionResult?: Function;
+    errorCallback?: Function;
+}
--- src/spectra-pitch-perfect-plugin.android.ts
+++ src/spectra-pitch-perfect-plugin.android.ts
@@ -6,25 +6,26 @@ export class SpectraPitchPerfectPlugin extends Common {
 }
 */
 
-import * as app from 'tns-core-modules/application';
+// import * as app from 'tns-core-modules/application';
+import {SpectraPitchPerfectOptions} from "./options";
 
 declare var org: any;
 
 export class SpectraPitchPerfectPlugin {
 
     private _pitchPerfectAsyncTask: any;
-    public start(): Promise<any> {
-        console.log('starting!!!!');
+    public start(options: SpectraPitchPerfectOptions): Promise<any> {
+        console.log('Pitch Perfect Plugin Starting.');
         return new Promise(async (resolve, reject) => {
             try {
                 // @ts-ignore
                 this._pitchPerfectAsyncTask =
                     new org.eu.transfusion.spectrapitchdetector.PitchDetector.PitchDetectorAsyncTask(
                         new org.eu.transfusion.spectrapitchdetector.OnPitchDetectionListener({
-                            onPitchDetectionResult: (pitchHz: Number, probability: Number, isPitched: Boolean) => {
+                            onPitchDetectionResult: options.onPitchDetectionResult/*: (pitchHz: Number, probability: Number, isPitched: Boolean) => {
                                 console.log('pitch acquired!!');
                                 console.log(pitchHz + ' ' + probability + ' ' + isPitched);
-                            }
+                            }*/
                         }));
                 this._pitchPerfectAsyncTask.execute(null);
             } catch (ex) {
@@ -46,3 +47,4 @@ export class SpectraPitchPerfectPlugin {
         });
     }
 }
+
